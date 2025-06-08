b859217b55660dd15d3abac3d73c8b5c26b3ac22
Osvaldo Maulén <osvaldo.maulen.riquelme@estudiante.ipss.cl>
2025-06-08 01:56:28 -0400

    add navbar, container

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+//IMPORT CSS
 import "./components/css/App.css";
 //IMPORT COMPONENTS
 import Footer from "./components/Footer";
 //IMPORT PAGES
 import Licitaciones from "./components/pages/Licitaciones";
+//IMPORT NAVBAR
+import Navbar from "./components/Navbar";
+//IMPORT CONTAINER
+import Container from "react-bootstrap/Container";
 
 function App() {
   return (
     <BrowserRouter>
       <>
+        <Navbar />
+        <Container className="talleres-container d-flex flex-column min-vh-100 justify-content-center align-items-center">
         <Routes>
           <Route path="/" element={<h1>Hola mundo</h1>} />
           <Route path="/licitaciones" element={<Licitaciones />} />
         </Routes>
+        </Container>
         <Footer />
       </>
     </BrowserRouter>
--- src/components/pages/Licitaciones.jsx
+++ src/components/pages/Licitaciones.jsx
@@ -1,9 +1,9 @@
-import { Container } from "react-bootstrap";
+import Container from "react-bootstrap";
 
 const Licitaciones = () => {
   return (
     <>
-      <Container>
+      <Container className="talleres-container d-flex flex-column min-vh-100 justify-content-center align-items-center">
         <h1>hsdahjkds</h1>
       </Container>
     </>
