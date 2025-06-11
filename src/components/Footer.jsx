3bd80af1458045420a9b1e1adb35e73dfbe9d4d1
Sebastian Lucero <seba.luceroguzman@gmail.com>
2025-06-11 16:54:13 -0400

    Termino de home y css en footer|NavBar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,29 +6,18 @@ import "./css/Footer.css";
 
 const Footer = () => {
   return (
-    <footer className="bg-dark text-light py-4 mt-auto">
+    <footer className="footer-navbar-style py-4 mt-auto">
       <Container>
         <Row>
-          <Col md={3} className="mb-3">
-            <img
-              src={Logo}
-              alt=""
-              style={{ width: "150px", marginBottom: "10px" }}
-              className="img-fluid"
-            />
+          <Col md={3} className="mb-3 d-flex flex-column align-items-start">
+            <img src={Logo} alt="Logo Chile" className="footer-logo mb-2" />
             <div>
-              <strong>Dirección ChileCompras</strong>
+              <strong>Dirección LicitaSeguros </strong>
               <br />
               Ministerio de Hacienda, Gobierno de Chile
               <br />
-              Monjitas 392 - Piso 8, Santiago de Chile
+              Av. Innovación 1234, Oficina 801, Santiago Centro
               <br />
-              <a
-                href="https://www.chilecompra.cl/terminos-y-condiciones-de-uso/"
-                className="text-light text-decoration-underline"
-              >
-                Términos y condiciones
-              </a>
             </div>
           </Col>
           <Col md={3} className="mb-3">
@@ -37,7 +26,7 @@ const Footer = () => {
               <li>
                 <a
                   href="https://comprador.mercadopublico.cl/registrarme"
-                  className="text-light text-decoration-underline"
+                  className="footer-link"
                 >
                   Registro nuevos organismos compradores
                 </a>
@@ -45,7 +34,7 @@ const Footer = () => {
               <li>
                 <a
                   href="https://datos-abiertos.chilecompra.cl/"
-                  className="text-light text-decoration-underline"
+                  className="footer-link"
                 >
                   Datos Abiertos ChileCompra
                 </a>
@@ -53,7 +42,7 @@ const Footer = () => {
               <li>
                 <a
                   href="https://capacitacion.chilecompra.cl/"
-                  className="text-light text-decoration-underline"
+                  className="footer-link"
                 >
                   Capacitación
                 </a>
@@ -61,9 +50,17 @@ const Footer = () => {
               <li>
                 <a
                   href="https://ayuda.mercadopublico.cl/"
-                  className="text-light text-decoration-underline"
+                  className="footer-link"
                 >
                   Centro de Ayuda
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.chilecompra.cl/terminos-y-condiciones-de-uso/"
+                  className="footer-link"
+                >
+                  Términos y condiciones
                 </a>
               </li>
             </ul>
@@ -71,17 +68,11 @@ const Footer = () => {
           <Col md={3} className="mb-3">
             <strong>Mesa de ayuda</strong>
             <div>
-              <a
-                href="tel:6007000600"
-                className="text-light text-decoration-underline"
-              >
+              <a href="tel:6007000600" className="footer-link">
                 600 7000 600
               </a>
               <br />
-              <a
-                href="tel:+56442360646"
-                className="text-light text-decoration-underline"
-              >
+              <a href="tel:+56442360646" className="footer-link">
                 +56 4 4236 0646
               </a>
             </div>
@@ -91,28 +82,28 @@ const Footer = () => {
             <div className="mb-2">
               <a
                 href="https://web.facebook.com/people/ChileCompra/100064656435287/?_rdc=1&_rdr#"
-                className="text-light me-2"
+                className="footer-link me-2"
               >
                 <i className="bi bi-facebook"></i>
               </a>
-              <a href="https://x.com/chilecompra" className="text-light me-2">
+              <a href="https://x.com/chilecompra" className="footer-link me-2">
                 <i className="bi bi-twitter"></i>
               </a>
               <a
-                href="linkedin.com/company/chilecompra?originalSubdomain=cl"
-                className="text-light me-2"
+                href="https://linkedin.com/company/chilecompra?originalSubdomain=cl"
+                className="footer-link me-2"
               >
                 <i className="bi bi-linkedin"></i>
               </a>
               <a
                 href="https://www.youtube.com/user/chilecompratv"
-                className="text-light me-2"
+                className="footer-link me-2"
               >
                 <i className="bi bi-youtube"></i>
               </a>
               <a
                 href="https://www.instagram.com/chilecompra/"
-                className="text-light"
+                className="footer-link"
               >
                 <i className="bi bi-instagram"></i>
               </a>
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -6,132 +6,30 @@ const Home = () => {
   return (
     <Container className="py-5">
       <Row className="justify-content-center my-4">
-        <Col xs={12} className="text-center mb-4">
-          <h1 className="home-title">
-            Encuentra hasta 4930 oportunidades de negocio en todo Chile
-          </h1>
-        </Col>
-        {/* Primera fila de 4 botones */}
-        <Col xs={12}>
-          <Row className="justify-content-center mb-2">
-            <Col xs={6} sm={3} md={3} className="mb-2 d-flex">
-              <Button
-                as="a"
-                href="https://www.mercadopublico.cl/Portal/Modules/Site/Busquedas/BuscadorAvanzado.aspx?qs=2"
-                target="_blank"
-                rel="noopener noreferrer"
-                variant="outline-secondary"
-                size="sm"
-                className="w-100 home-btn"
-              >
-                Órdenes de compra
-              </Button>
-            </Col>
-            <Col xs={6} sm={3} md={3} className="mb-2 d-flex">
-              <Button
-                as="a"
-                href="https://buscador.mercadopublico.cl/compra-agil?date_from=2025-05-12&date_to=2025-06-11&order_by=recent&page_number=1&region=all&status=2"
-                target="_blank"
-                rel="noopener noreferrer"
-                variant="outline-secondary"
-                size="sm"
-                className="w-100 home-btn"
-              >
-                Compra Ágil
-              </Button>
-            </Col>
-            <Col xs={6} sm={3} md={3} className="mb-2 d-flex">
-              <Button
-                as="a"
-                href="https://www.mercadopublico.cl/Contratos/Ciudadania"
-                target="_blank"
-                rel="noopener noreferrer"
-                variant="outline-secondary"
-                size="sm"
-                className="w-100 home-btn"
-              >
-                Contratos
-              </Button>
-            </Col>
-            <Col xs={6} sm={3} md={3} className="mb-2 d-flex">
-              <Button
-                as="a"
-                href="https://consulta-mercado.mercadopublico.cl/buscador-publico?dateFrom=11-03-2025&dateTo=11-06-2025&order=100&page=1&status=100"
-                target="_blank"
-                rel="noopener noreferrer"
-                variant="outline-secondary"
-                size="sm"
-                className="w-100 home-btn"
-              >
-                Consultas al mercado
-              </Button>
-            </Col>
-          </Row>
-        </Col>
-        {/* Segunda fila de 4 botones */}
-        <Col xs={12}>
-          <Row className="justify-content-center mb-3">
-            <Col xs={6} sm={3} md={3} className="mb-2 d-flex">
-              <Button
-                as="a"
-                href="https://www.mercadopublico.cl/Portal/Modules/Site/Busquedas/BuscadorAvanzado.aspx?qs=9"
-                target="_blank"
-                rel="noopener noreferrer"
-                variant="outline-secondary"
-                size="sm"
-                className="w-100 home-btn"
-              >
-                Grandes compras
-              </Button>
-            </Col>
-            <Col xs={6} sm={3} md={3} className="mb-2 d-flex">
-              <Button
-                as="a"
-                href="https://trato-directo.mercadopublico.cl/buscador-publico?dateFrom=04%2F06%2F2025&dateTo=11%2F06%2F2025&orderBy=1&page=0&size=10"
-                target="_blank"
-                rel="noopener noreferrer"
-                variant="outline-secondary"
-                size="sm"
-                className="w-100 home-btn"
-              >
-                Trato directo
-              </Button>
-            </Col>
-            <Col xs={6} sm={3} md={3} className="mb-2 d-flex">
-              <Button
-                as="a"
-                href="https://proveedor.mercadopublico.cl/busqueda"
-                target="_blank"
-                rel="noopener noreferrer"
-                variant="outline-secondary"
-                size="sm"
-                className="w-100 home-btn"
-              >
-                Proveedores
-              </Button>
-            </Col>
-            <Col xs={6} sm={3} md={3} className="mb-2 d-flex">
-              <Button
-                as="a"
-                href="https://comprador.mercadopublico.cl/busqueda"
-                target="_blank"
-                rel="noopener noreferrer"
-                variant="outline-secondary"
-                size="sm"
-                className="w-100 home-btn"
-              >
-                Compradores
-              </Button>
-            </Col>
-          </Row>
-        </Col>
         <Col xs={12} className="text-center">
-          <h2 className="home-subtitle">Bienvenido a LicitaSeguros</h2>
-          <p className="home-desc">
-            Plataforma para consultar licitaciones, proveedores y más.
-          </p>
-          <img src={Region} alt="img" className="home-img" />
+          <h1 className="home-subtitle">Bienvenido a LicitaSeguros</h1>
         </Col>
+        {/* INFORMACION LICITASEGUROS */}
+        <Row className="justify-content-center my-5">
+          <Col md={8} sm={12}>
+            <h2 className="info-title">¿Qué es LicitaSeguros?</h2>
+            <p className="info-desc">
+              <b>LicitaSeguros</b> es una plataforma que facilita la consulta y
+              el acceso a información relevante sobre licitaciones públicas,
+              proveedores y oportunidades de negocio en Chile. Nuestro objetivo
+              es simplificar el proceso de búsqueda y análisis de licitaciones,
+              permitiendo a empresas y personas encontrar oportunidades de
+              manera rápida y eficiente.
+            </p>
+            <p className="info-desc">
+              Aquí podrás explorar licitaciones activas, consultar detalles
+              específicos, buscar proveedores y acceder a recursos útiles para
+              participar en el mercado público. LicitaSeguros centraliza la
+              información clave para que tomes mejores decisiones y aproveches
+              al máximo las oportunidades disponibles.
+            </p>
+          </Col>
+        </Row>
       </Row>
       <Row className="g-4 mt-2">
         <Col md={4} sm={12}>
@@ -190,6 +88,124 @@ const Home = () => {
           </Card>
         </Col>
       </Row>
+      <Col xs={12} className="text-center">
+        <img src={Region} alt="img" className="home-img" />
+      </Col>
+      {/* Primera fila de 4 botones */}
+      <h2 className="info-title text-center">Links Mercado Publico</h2>
+      <Col xs={12}>
+        <Row className="justify-content-center mb-2">
+          <Col xs={6} sm={3} md={3} className="mb-2 d-flex">
+            <Button
+              as="a"
+              href="https://www.mercadopublico.cl/Portal/Modules/Site/Busquedas/BuscadorAvanzado.aspx?qs=2"
+              target="_blank"
+              rel="noopener noreferrer"
+              variant="outline-secondary"
+              size="sm"
+              className="w-100 home-btn"
+            >
+              Órdenes de compra
+            </Button>
+          </Col>
+          <Col xs={6} sm={3} md={3} className="mb-2 d-flex">
+            <Button
+              as="a"
+              href="https://buscador.mercadopublico.cl/compra-agil?date_from=2025-05-12&date_to=2025-06-11&order_by=recent&page_number=1&region=all&status=2"
+              target="_blank"
+              rel="noopener noreferrer"
+              variant="outline-secondary"
+              size="sm"
+              className="w-100 home-btn"
+            >
+              Compra Ágil
+            </Button>
+          </Col>
+          <Col xs={6} sm={3} md={3} className="mb-2 d-flex">
+            <Button
+              as="a"
+              href="https://www.mercadopublico.cl/Contratos/Ciudadania"
+              target="_blank"
+              rel="noopener noreferrer"
+              variant="outline-secondary"
+              size="sm"
+              className="w-100 home-btn"
+            >
+              Contratos
+            </Button>
+          </Col>
+          <Col xs={6} sm={3} md={3} className="mb-2 d-flex">
+            <Button
+              as="a"
+              href="https://consulta-mercado.mercadopublico.cl/buscador-publico?dateFrom=11-03-2025&dateTo=11-06-2025&order=100&page=1&status=100"
+              target="_blank"
+              rel="noopener noreferrer"
+              variant="outline-secondary"
+              size="sm"
+              className="w-100 home-btn"
+            >
+              Consultas al mercado
+            </Button>
+          </Col>
+        </Row>
+      </Col>
+      {/* Segunda fila de 4 botones */}
+      <Col xs={12}>
+        <Row className="justify-content-center mb-3">
+          <Col xs={6} sm={3} md={3} className="mb-2 d-flex">
+            <Button
+              as="a"
+              href="https://www.mercadopublico.cl/Portal/Modules/Site/Busquedas/BuscadorAvanzado.aspx?qs=9"
+              target="_blank"
+              rel="noopener noreferrer"
+              variant="outline-secondary"
+              size="sm"
+              className="w-100 home-btn"
+            >
+              Grandes compras
+            </Button>
+          </Col>
+          <Col xs={6} sm={3} md={3} className="mb-2 d-flex">
+            <Button
+              as="a"
+              href="https://trato-directo.mercadopublico.cl/buscador-publico?dateFrom=04%2F06%2F2025&dateTo=11%2F06%2F2025&orderBy=1&page=0&size=10"
+              target="_blank"
+              rel="noopener noreferrer"
+              variant="outline-secondary"
+              size="sm"
+              className="w-100 home-btn"
+            >
+              Trato directo
+            </Button>
+          </Col>
+          <Col xs={6} sm={3} md={3} className="mb-2 d-flex">
+            <Button
+              as="a"
+              href="https://proveedor.mercadopublico.cl/busqueda"
+              target="_blank"
+              rel="noopener noreferrer"
+              variant="outline-secondary"
+              size="sm"
+              className="w-100 home-btn"
+            >
+              Proveedores
+            </Button>
+          </Col>
+          <Col xs={6} sm={3} md={3} className="mb-2 d-flex">
+            <Button
+              as="a"
+              href="https://comprador.mercadopublico.cl/busqueda"
+              target="_blank"
+              rel="noopener noreferrer"
+              variant="outline-secondary"
+              size="sm"
+              className="w-100 home-btn"
+            >
+              Compradores
+            </Button>
+          </Col>
+        </Row>
+      </Col>
     </Container>
   );
 };
